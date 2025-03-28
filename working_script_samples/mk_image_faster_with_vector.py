1bf6161bfe49e218eb407f4bb718e87c37354edb
ryuken73 <ryuken73@gmail.com>
2025-03-29 04:45:22 +0900

    update: make mono fd ir105 image using vector

--- working_script_samples/mk_image_faster_with_vector.py
+++ working_script_samples/mk_image_faster_with_vector.py
@@ -43,6 +43,18 @@ def get_color_from_temperature(temp):
     b = int(start_color[2] + (end_color[2] - start_color[2]) * ratio)
     return [r, g, b, 255]
 
+def get_mono_color_from_value(value):
+    if value == -9999:
+        return [0, 0, 0, 0]  # 투명
+        # print(f'fill red {value}')
+        return [255, 0, 0, 255]  # red
+    t_min, t_max = -100, 30
+    ratio = min(max((value - t_min) / (t_max - t_min), 0), 1)
+    r = int(255 * (1 - ratio))
+    g = int(255 * (1 - ratio))
+    b = int(255 * (1 - ratio))
+    return [r, g, b, 255]  # RGBA
+
 def generate_image_from_data_fast(data, output_path, image_size=(600, 520), bounds=[60, -80, 180, 80]):
     """
     NumPy 배열 [[lon, lat, value], ...] 데이터를 Web Mercator 투영으로 이미지 변환.
@@ -79,13 +91,16 @@ def generate_image_from_data_fast(data, output_path, image_size=(600, 520), boun
     grid_values = np.full((height, width), -9999, dtype=np.float32)
     grid_values[rows, cols] = values  # 값 매핑
 
-    print('grid_values sample:', grid_values[600:800])
+    print('grid_values sample:', grid_values[:5])
+    print("Image pixel values 범위:", np.min(grid_values), "to", np.max(grid_values))
 
     # 색상 매핑을 위한 벡터화 함수
     def apply_color_mapping(values):
         colors = np.zeros((height, width, 4), dtype=np.uint8)
         flat_values = values.flatten()
-        flat_colors = np.array([get_color_from_temperature(val) if val != -9999 else [0, 0, 0, 0] 
+        # flat_colors = np.array([get_color_from_temperature(val) if val != -9999 else [0, 0, 0, 0] 
+        #                         for val in flat_values])
+        flat_colors = np.array([get_mono_color_from_value(val) if val != -9999 else [0, 0, 0, 0] 
                                 for val in flat_values])
         colors = flat_colors.reshape(height, width, 4)
         return colors
@@ -94,7 +109,7 @@ def generate_image_from_data_fast(data, output_path, image_size=(600, 520), boun
     image_data = apply_color_mapping(grid_values)
     print('length of image_dat:', len(image_data))
     print('sample of image_dat:', image_data[:100])
-    print('sample of image_dat:', image_data[600:800])
+    print('sample of image_dat:', image_data[100:200])
     print('shape and size of image_dat:', image_data.shape, image_data.size, image_data.dtype)
 
     # PNG 저장
