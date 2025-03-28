5b1ccb19f5628cad6f0cd091be52a6d7190ee9fe
ryuken73 <ryuken73@gmail.com>
2025-03-29 04:46:34 +0900

    etc

--- working_script_samples/mk_smooth_image.py
+++ working_script_samples/mk_smooth_image.py
@@ -1,4 +1,6 @@
 from PIL import Image
-img = Image.open(r'D:\002.Code\099.study\deck.gl.test\public\output_image_fast_4096_17m_step1.png')
-img = img.resize((1400, 1400), Image.Resampling.LANCZOS)
-img.save(r'D:\002.Code\099.study\deck.gl.test\public\output_image_fast_4096_17m_1400_step1_smooth.png')
+img = Image.open(r'D:\002.Code\001.python\netcdf\working_script_samples\output_image_fast_w_vector_3192_5M.png')
+img2048 = img.resize((2048, 2048), Image.Resampling.LANCZOS)
+img1400 = img.resize((1400, 1400), Image.Resampling.LANCZOS)
+img2048.save(r'D:\002.Code\001.python\netcdf\working_script_samples\output_image_fast_w_vector_3192_5M_to_2048.png')
+img1400.save(r'D:\002.Code\001.python\netcdf\working_script_samples\output_image_fast_w_vector_3192_5M_to_1400.png')
